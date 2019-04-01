93eea47e74c98edec1aa10762641e316212d6fc4
luann2424t <luann2424t@gmail.com>
2019-04-01 20:30:16 +0700

    commit test

---
+++ application/views/layout/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        <h1>this is footer</h1>
+    </footer>
+</body>
+</html>
---
+++ application/views/layout/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link rel='stylesheet' id='tablepress-default-css'  href='/application/views/css/style.css' type='text/css' media='all' />
+
+</head>
+<body>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WZ34NK5"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+
+	<div class="container"><!--CONTAINER-->
+		<header><!--HEADER-->
+			<a class="logo logoEn" href="home.html"></a>
+            <div class="clearfix" style = 'margin-bottom: 125px'></div>
+			<nav class="desktop desktop-en"><!--NAV-->
+				<ul>
+                    <li id="menu-item-1985" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1985">
+                        <a href="Home.html">Home</a>
+                    </li>
+					<li id="menu-item-1985" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1985">
+                        <a href="Category.html">Category</a>
+                    </li>
+                    </li>
+                    <li id="menu-item-1985" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1985">
+                            <a href="Booking.html">Booking</a>
+                    </li>
+                    <li id="menu-item-1985" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-1985">
+                        <a href="Event.html">Event</a>
+                    </li>
+				</ul>
+			</nav>
+		</header><!--HEADER-->
