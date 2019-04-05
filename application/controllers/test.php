bb68b6c8a892dea918a50d8640a47a1df20e7bae
luann2424t <luann2424t@gmail.com>
2019-04-05 19:17:09 +0700

    commit

--- application/controllers/test.php
+++ application/controllers/test.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Test extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,10 +18,24 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function __construct()
+	{
+		parent::__construct();
+		// $this->load->model('animalmodel');
+		// $this->load->model('bookingmodel');
+		// $this->load->model('categorymodel');
+		// $this->load->model('eventmodel');
+		// $this->load->model('feedbackmodel');
+		$this->load->helper('html');
+		$this->load->helper('url');
+	}
 	public function index()
 	{
 		$this->load->view('layout/header');
 		// $this->load->view('welcome_message');
 		$this->load->view('layout/footer');
+	}
+	public function ttt() {
+		$this->load->view('layout/header');
 	}
 }
